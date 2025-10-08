685c917cc094c6150df73de16ad342e86e6b37cd
sloobman <user@email.com>
2025-10-08 22:07:23 +0300

    stats.py modified added base settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,13 @@
+import os
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
